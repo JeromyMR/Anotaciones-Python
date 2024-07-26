56835de9fcbedc316884e0fd36c8bffb3f73b680
JeromyMR <135396889+JeromyMR@users.noreply.github.com>
2024-07-26 10:12:10 -0400

    Delete Condicionales (if , else).py

--- Condicionales (if , else).py
+++
@@ -1,8 +0,0 @@
-usuario_de_base_de_datos = "Jeremy Marin"
-usuario_escrito = "Jeremy Marin"
-
-if usuario_de_base_de_datos == usuario_escrito:
-    print ("Ingresando a la plataforma")
-
-else:
-    print("El usuario se encuentra equivocado")
